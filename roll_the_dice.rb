df8c41fce8875ae0e913f33a904dcabb5383a82a
Vladimire87 <v.egorov@bslimousine.com>
2023-01-11 12:43:34 +0400

    Add functionality to roll multiple dice

--- roll_the_dice.rb
+++ roll_the_dice.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
-dice = rand(1..6)
-puts dice
+def show_roll
+  100.times do
+    print "#{rand(1..6)}\r"
+    sleep 0.01
+  end
+end
+
+puts 'Сколько кубиков кидаем?'
+number = gets.to_i
+
+number.times do
+  show_roll
+  puts rand(1..6)
+end
